43eb35a1c2a975d47188c3babd91d0ec59b888d7
deloosecr <deloosecr@ncias-d1611-v.nci.nih.gov>
2015-11-02 22:41:30 +0000

    CD: Pushing BRP redesign, pt 2 Former-commit-id: abe5bbc622b08d477d27cdc53fa8e5267573b4cf

---
+++ brp/js/ncintwk-biopthwys.js
@@ -0,0 +1,148 @@
+var terms = [
+	{
+		'term': 'one',
+		'title': '1. Effects of Cognitive  Behavioral Stress Management Intervention on Clinical Disease Endpoints in  Women with Breast Cancer',
+		'definition': '<p><strong>Michael H. Antoni, Ph.D.,  University of Miami </strong></p><p>This project consists of extensive follow-up of a randomized, controlled  trial, which aims to test the effects of group-based cognitive behavioral  stress management intervention versus psychoeducational control on clinical  disease endpoints over an extended time in women initially treated for  non-metastatic breast cancer. It examines whether specific biobehavioral and  psychological adaptation processes modified by the intervention in the first  year of medical treatment predict differences in clinical endpoints (e.g.,  survival and recurrence) up to 13 years later. </p>',
+		'contacts': 'Michael H. Antoni, Ph.D., University of Miami<br><br>This project consists of extensive follow-up of a randomized, controlled trial, which aims to test the effects of group-based cognitive behavioral stress management intervention versus psychoeducational control on clinical disease endpoints over an extended time in women initially treated for non-metastatic breast cancer. It examines whether specific biobehavioral and psychological adaptation processes modified by the intervention in the first year of medical treatment predict differences in clinical endpoints (e.g., survival and recurrence) up to 13 years later.'	
+	}, {
+		'term': 'two',
+		'title': '2. Perioperative Use of Beta-blocker and COX2 Inhibitor ',
+		'definition': '<p><strong>Shamgar  Ben-Eliyahu, Ph.D., Tel Aviv University </strong> </p><p>This project uses multicenter randomization to assess the short-term  effects of counteracting excess perioperative catecholamine and prostaglandin  levels on immune, endocrine, and pro-metastatic serum and histological indices.  Results of this project will be used to develop a large-scale clinical trial. </p>',
+		'contacts': 'Shamgar Ben-Eliyahu, Ph.D., Tel Aviv University<br><br>This project uses multicenter randomization to assess the short-term effects of counteracting excess perioperative catecholamine and prostaglandin levels on immune, endocrine, and pro-metastatic serum and histological indices. Results of this project will be used to develop a large-scale clinical trial.'
+	}, {
+		'term': 'three',
+		'title': '3. Behavioral Pathways in Stem  Cell Transplantation: CD14+ Gene Expression',
+		'definition': '<p><strong>Erin  Costanzo, Ph.D., University of Wisconsin, Madison </strong> </p><p>This project examines gene expression patterns of CD14+ cells as a potential pathway by which stress-related psychosocial factors may influence immune recovery and clinical outcomes following hematopoietic stem cell transplantation (HSCT). The study focuses on multiple myeloma patients recovering from autologous HSCT, given evidence for the role of CD14+ derived macrophages in immune recovery as well as in angiogenesis, apoptosis, and production of disease-promoting cytokines in this population.</p>',
+		'contacts': 'Erin Costanzo, Ph.D., University of Wisconsin, Madison<br><br>This project examines gene expression patterns of CD14+ cells as a potential pathway by which stress-related psychosocial factors may influence immune recovery and clinical outcomes following hematopoietic stem cell transplantation (HSCT). The study focuses on multiple myeloma patients recovering from autologous HSCT, given evidence for the role of CD14+ derived macrophages in immune recovery as well as in angiogenesis, apoptosis, and production of disease-promoting cytokines in this population.'
+	}, {
+		'term': 'four',
+		'title': '4. Neural Substrates of Social  Support',
+		'definition': '<p><strong>Naomi  Eisenberger, Ph.D., University of California, Los Angeles</strong></p><p>  This project produced a white paper that served as the basis for the  fMRI probe optimization study. The paper provides a literature review of  fMRI-based research on behavioral probe paradigms and empirical neural activity  correlates of social support, attachment, affiliation, and related psychological  constructs. <br />See Eisenberger, N. (2013). An Empirical Review of the Neural  Underpinnings of Receiving and Giving Social Support: Implications for Health.  Psychosomatic Medicine, 75(6):545–556. <a href="http://journals.lww.com/psychosomaticmedicine/Abstract/2013/07000/An_Empirical_Review_of_the_Neural_Underpinnings_of.6.aspx">http://journals.lww.com/psychosomaticmedicine/Abstract/2013/07000/An_Empirical_Review_of_the_Neural_Underpinnings_of.6.aspx</a> <a href="/exit_disclaimer.html"><img alt="Exit Disclaimer" src="/images/Icon_External_Link.png"></a></p>',
+		'contacts': 'Naomi Eisenberger, Ph.D., University of California, Los Angeles<br><br>This project produced a white paper that served as the basis for the fMRI probe optimization study. It provided a literature review of fMRI-based research on behavioral probe paradigms and empirical neural activity correlates of social support, attachment, affiliation, and related psychological constructs.<br><br>See Eisenberger, N. (2013). An Empirical Review of the Neural Underpinnings of Receiving and Giving Social Support: Implications for Health. Psychosomatic Medicine, 75(6):545–556.'
+	}, {
+		'term': 'five',
+		'title': '5. fMRI Probe Optimization  Study of Social Support',
+		'definition': '<p><strong>Naomi  Eisenberger, Ph.D., and Julienne Bower, Ph.D., University of California, Los  Angeles </strong> </p><p>  Based on the white paper referenced above, this project aims to identify  and pilot test fMRI probe tasks that can help assess whether individual  differences in measures of experienced social support (as measured by the  Social Provisions Scale-Attachment and the UCLA Loneliness Scale) relate systematically  to individual differences in neural activity to the specialized probe tasks.  Phase I of the project will be conducted in healthy volunteer subjects. </p>',
+		'contacts': 'Naomi Eisenberger, Ph.D., University of California, Los Angeles<br><br>Based on the white paper referenced above, this project will identify and pilot test fMRI probe tasks that can help capture systematic individual differences in neural activity associated with performance of probe tasks among individuals who differ in relevant measures of experienced social support (as measured by the Social Provisions Scale-Attachment and the UCLA Loneliness Scale). Phase I of the project will be conducted in healthy volunteer subjects from undergraduate institutions.'
+	}, {
+		'term': 'six',
+		'title': '6. Stress and Tumorigenesis in  Normal and p53 Knockout Models',
+		'definition': '<p><strong>Wenwei  Hu, Ph.D., Robert Wood Johnson Foundation </strong> </p><p>  This project is investigating the impact of chronic stress on DNA damage  accumulation, which, in keeping with the established chronic stress mouse model  system, then promotes tumor initiation. Based on the recent finding that  chronic stress attenuates the function of p53, a central player in tumor  suppression, this project will test whether chronic stress promotes DNA damage,  enhances mutation frequency and copy number variation in a largely  p53-dependent manner. </p>',
+		'contacts': 'Wenwei Hu, Ph.D., Robert Wood Johnson Foundation<br><br>This project is investigating the impact of chronic stress on DNA damage accumulation, which in turn will promote tumor initiation by employing the established chronic stress mouse model system.'
+	}, {
+		'term': 'seven',
+		'title': '7. Beta-Adrenergic Regulation of  Acute Lymphocytic Leukemia',
+		'definition': '<p><strong>Don  Lamkin, Ph.D., University of California, Los Angeles </strong> </p><p>  This project examines a potential downstream mechanism by which  beta-adrenergic signaling accelerates progression of acute lymphoblastic  leukemia (ALL). Specifically, an orthotopic mouse model of human leukemia will  be used to understand the role of the CXCR4- CXCL12 chemokine system in  stress-enhanced ALL progression. </p>',
+		'contacts': 'Don Lamkin, Ph.D., University of California, Los Angeles<br><br>This project examines a potential downstream mechanism by which beta-adrenergic signaling regulates stress-induced acute lymphoblastic leukemia (ALL). Specifically, an orthotopic mouse model of human leukemia will be used to understand the role of the CXCR4-CXCL12 chemokine system in stress-enhanced ALL progression.'
+	}, {
+		'term': 'eight',
+		'title': '8. Metabolic Fingerprint of  Stress Influences in Cancer',
+		'definition': '<p><strong>Anil  K. Sood, MD, MD Anderson Cancer Center </strong> </p><p>  This project examines unique metabolic signatures in epithelial ovarian  cancer tissues from human subjects with different depression and social support  profiles. Global biochemical profiles were studied in tissue samples (n=85)  from patients who are categorized according to scores on biobehavioral risk  factors. Mechanistic and biological studies to examine the regulation of  metabolic pathways in the context of chronic stress effects on the tumor  microenvironment were also conducted. </p>',
+		'contacts': 'Anil K. Sood, MD, M.D. Anderson Cancer Center<br><br>This project examined unique metabolic signatures in epithelial ovarian cancer tissues from human subjects with different depression and social support profiles. Global biochemical profiles were examined in tissue samples (n=85) from patients who are categorized according to scores on biobehavioral risk factors. Mechanistic and biological studies to examine the regulation of metabolic pathways in the context of chronic stress effects on the tumor microenvironment were also conducted.'
+	}, {
+		'term': 'nine',
+		'title': '9. Interactive Wiki Page',
+		'definition': '<p><strong>Tor  Wager, Ph.D., University of Colorado, Boulder </strong> </p><p>  This project provides a targeted literature review of fMRI-based  research on brain stem control of sympathetic nervous system (SNS) activity,  and its regulation by higher brain (e.g., cortical) regions. The interactive  wiki page (<a href="http://brainstemwiki.colorado.edu">http://brainstemwiki.colorado.edu</a> <a href="/exit_disclaimer.html"><img alt="Exit Disclaimer" src="/images/Icon_External_Link.png"></a>) identifies cortical and  sub-cortical brain regions of interest and the relevant fMRI probe paradigm  tasks suitable for use in subsequent studies relating social support to SNS  activation. </p>',
+		'contacts': 'Tor Wager, Ph.D., University of Colorado, Boulder<br><br>This project provides a targeted literature review of fMRI-based research on brain stem control of sympathetic nervous system (SNS) activity, and its regulation by higher brain (e.g., cortical) regions. The interactive wiki page (<a href="http://brainstemwiki.colorado.edu">http://brainstemwiki.colorado.edu</a> <a href="/exit_disclaimer.html"><img alt="Exit Disclaimer" src="/images/Icon_External_Link.png"></a>) identifies cortical and sub-cortical brain regions of interest and the relevant fMRI probe paradigm tasks suitable for use in subsequent studies relating social support to SNS activation.'
+	}, {
+		'term': 'ten',
+		'title': '10. Biobehavioral Benefits of  Mixed Gender Housing on Survival of Mice with Colon Neoplasia',
+		'definition': '<p><strong>Nathan  A. Berger, MD, Case Western Reserve University </strong> </p><p>  To determine mechanisms and provide potential targets for design and  implementation of biobehavioral interventions in cancer patients and survivors,  this project examines the effect of (1) mixed gender housing, (2) same gender  housing, and (3) social isolation on survival and changes in circulating  hormones, cytokines, and other mediators and biomarkers in C57BL6J mice bearing  the APCMin mutation predisposing to gastrointestinal neoplasia.</p>',
+		'contacts': 'Nathan A. Berger, MD, Case Western Reserve University<br><br>This project examines the effect of (1) mixed gender housing, (2) same gender housing, and (3) social isolation on survival and changes in circulating hormones, cytokines, and other mediators and biomarkers in C57BL6J mice bearing the APCMin mutation predisposing to gastrointestinal neoplasia.'
+	}, {
+		'term': 'eleven',
+		'title': '11. Dual Psychosocial Stressor  Exposure and Spontaneous Breast Tumor Metastasis in MMTV-PyMT Mice: Does Dual  Stressor Exposure Increase Metastasis by Regulating Circulating Exosomes and  Tumor Extracellular Matrix?',
+		'definition': '<p><strong>Kelley  S. Madden, Ph.D., University of Rochester </strong> </p><p>  This project will establish how exposure to a dual stressor is linked to  metastasis in MMTV-PyMT mice, a spontaneous breast cancer model that mimics  metastatic breast cancer in humans. The stressor, a combination of chronic  psychological stress (social isolation) and acute restraint stress, may mimic  the complex stressors experienced by cancer patients. This project examines  distinct and novel biological mechanisms underlying stress exposure: (1)  circulating exosomes, a newly defined metastatic mechanism, and (2) alterations  in the tumor extracellular matrix. </p>',
+		'contacts': 'Kelly S. Madden, Ph.D., University of Rochester<br><br>This project will establish how exposure to a dual stressor is linked to metastasis in MMTV-PyMT mice, a spontaneous breast cancer model that mimics metastatic breast cancer in humans. The stressor, a combination of chronic psychological stress (social isolation) and acute restraint stress, may mimic the complex stressors experienced by cancer patients. This project examines distinct and novel biological mechanisms underlying stress exposure: (1) circulating exosomes, a newly defined metastatic mechanism, and (2) alterations in the tumor extracellular matrix.'
+	}, {
+		'term': 'twelve',
+		'title': '12. Using Propranolol to  Decrease Gene Expression of Stress- Mediated Beta-Adrenergic Pathways in  Hematopoietic Stem Cell Transplant Recipients',
+		'definition': '<p><strong>Jennifer  M. Knight, MD, Medical College of Wisconsin</strong></p><p>This project assesses  whether gene expression of beta-adrenergic signaling pathways can be altered in  individuals undergoing HSCT for multiple myeloma by administering a daily beta-blocker  (propranolol). This is the first human trial of a beta-blocker to improve  cancer control by pharmacologically intervening in biobehaviorally mediated  gene expression. The goal is to leverage these findings for a larger,  randomized controlled trial that evaluates clinical outcomes.</p>',
+		'contacts': 'Jennifer M. Knight, MD, Medical College of Wisconsin<br><br>This project assesses whether gene expression of beta-adrenergic signaling pathways can be altered in individuals undergoing HSCT for multiple myeloma by administering a daily beta-blocker (propranolol). This is the first human trial of a beta-blocker to improve cancer control by pharmacologically intervening in biobehaviorally mediated gene expression. The goal is to leverage these findings for a larger, randomized controlled trial evaluating clinical outcomes.'
+	}
+];
+
+var windowsize = $(window).width();
+
+$(document).resize(function() {
+  var windowsize = $(window).width();
+});
+
+$(document).ready(function() {
+	var p_div = $('#popup'),
+		a_div = $('#popup-acro'),
+		c_div = $('.contact-info');
+
+	// user interaction (image) - hover
+	$('area').mouseenter(function(e) {
+		var x = $(this).attr('id'),
+			term_content = '',
+			moveLeft = 0,
+			moveDown = 0;
+						
+		if (x === 'one' || x === 'two' || x === 'nine' || x === 'ten' || x === 'eleven' || x === 'twelve') {
+				moveLeft = 10,
+				moveDown = -70;
+			} else if (x === 'three' || x === 'four' || x === 'five' || x === 'six' || x === 'seven' || x === 'eight') {
+				moveLeft = -400,
+				moveDown = -50;
+			} else {
+			moveLeft = -140,
+			moveDown = 30;
+		}
+		
+		if (windowsize < 768) {
+			if (x === 'one' || x === 'two' || x === 'nine' || x === 'ten' || x === 'eleven' || x === 'twelve') {
+				moveLeft = 50,
+				moveDown = -50;
+			} else if (x === 'three' || x === 'four' || x === 'five' || x === 'six' || x === 'seven' || x === 'eight') {
+				moveLeft = -400,
+				moveDown = -50;
+			} else {
+			}
+		}
+		
+		term_content='Click on the Investigator&rsquo;s photo to see their profile.';
+		
+			if (windowsize > 490) {
+		for(var i = 0; i < terms.length; i++) {
+			if(terms[i].term == x) { term_content = '<h3>' + terms[i].title + '</h3>' + '<p>' + terms[i].definition + '</p>' }
+		}
+		p_div.empty();
+		p_div.append(term_content);
+		p_div.stop(true).fadeTo(0, 1.0);
+
+		$('area').mousemove(function(e) { p_div.css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft) });	
+	}});
+	$('area').mouseleave(function(e) { p_div.fadeOut(0) });
+
+	// user interaction - click
+	$('area').click(function() {
+		var y = $(this).attr('id');
+		for(var i = 0; i < terms.length; i++) {
+			if(terms[i].term == y) {
+				contact_content = '<h2>' + terms[i].title + '</h2>';
+				contact_content += terms[i].definition;
+			}
+		}
+		$('#scroll').css('border','solid thin #ccc');
+		$('#scroll').css('padding','10px');
+		$('#scroll').css('margin-bottom','10px');
+		c_div.empty();
+		c_div.append(contact_content);
+	});
+});
+
+// scroll animation
+$(function() {
+	$('area[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({ scrollTop: target.offset().top }, 1200);
+				return false;
+			}
+		}
+	});
+});
