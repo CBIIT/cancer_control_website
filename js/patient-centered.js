3f1bfed5b75ef08878d1eea5baa5db42d9386828
deloosecr <deloosecr@ncias-d1611-v.nci.nih.gov>
2016-07-14 18:08:22 +0000

    Merged revision(s) 634-636 from branches/ICF
    Former-commit-id: 15fe8edc919a44773a917cfc6374e77a9fdaef56

---
+++ js/patient-centered.js
@@ -0,0 +1,119 @@
+var terms = [
+	{
+		'term': 'behavioral-research',
+		'title': 'Behavioral Research',
+		'definition': '',
+		'contacts': '<table summary="Contact information for Research platforms." class="table table-striped table-bordered table-condensed responsive"><thead><tr><th scope="col">Staff Member</th><th scope="col">Email</th></tr></thead><tbody><tr><td><a href="http://staffprofiles.cancer.gov/brp/prgmStaffProfile.do?contactId=993308&amp;bioType=stf">Wen-Ying Chou</a></td><td><a href="http://staffprofiles.cancer.gov/brp/prgmStaffProfile.do?contactId=993308&amp;bioType=stf">chouws@mail.nih.gov</a></td></tr><tr><td><a href="http://staffprofiles.cancer.gov/brp/prgmStaffProfile.do?contactId=480126&amp;bioType=stf">William Klein</a></td><td><a href="http://staffprofiles.cancer.gov/brp/prgmStaffProfile.do?contactId=480126&amp;bioType=stf">kleinwm@mail.nih.gov</a></td></tr><tr><td><a href="http://staffprofiles.cancer.gov/brp/prgmStaffProfile.do?contactId=1655410&amp;bioType=stf">James McClain</a></td><td><a href="http://staffprofiles.cancer.gov/brp/prgmStaffProfile.do?contactId=1655410&amp;bioType=stf">mcclainjj@mail.nih.gov</a></td></tr><tr><td><a href="http://staffprofiles.cancer.gov/brp/prgmStaffProfile.do?contactId=24750065&amp;bioType=flw">Megan Roberts</a></td><td><a href="http://staffprofiles.cancer.gov/brp/prgmStaffProfile.do?contactId=24750065&amp;bioType=flw">megan.roberts@nih.gov</a></td></tr></tbody></table>'
+	}, {
+		'term': 'epidemiology',
+		'title': 'Epidemiology and Genomics Research',
+		'definition': '',
+		'contacts': '<table summary="Contact information for Research platforms." class="table table-striped table-bordered table-condensed responsive"><thead><tr><th scope="col">Staff Member</th><th scope="col">Email</th></tr></thead><tbody><tr><td><a href="http://epi.grants.cancer.gov/staff/dathe.html">Marina Dathe</a></td><td><a href="http://epi.grants.cancer.gov/staff/dathe.html">marina.dathe@nih.gov</a></td></tr><tr><td><a href="http://epi.grants.cancer.gov/staff/kennedy.html">Amy Kennedy</a></td><td><a href="http://epi.grants.cancer.gov/staff/kennedy.html">amy.kennedy@nih.gov</a></td></tr><tr><td><a href="https://www.cdc.gov/genomics/about/staff/mkhoury.htm">Muin Khoury</a></td><td><a href="https://www.cdc.gov/genomics/about/staff/mkhoury.htm">muk1@cdc.gov</a></td></tr><tr><td><a href="http://epi.grants.cancer.gov/staff/marcus.html">Pam Marcus</a></td><td><a href="http://epi.grants.cancer.gov/staff/marcus.html">marcusp@mail.nih.gov</a></td></tr><tr><td><a href="http://epi.grants.cancer.gov/staff/martin.html">Damali Martin</a></td><td><a href="http://epi.grants.cancer.gov/staff/martin.html">martinda@mail.nih.gov</a></td></tr><tr><td><a href="http://epi.grants.cancer.gov/staff/reedy.html">Jill Reedy</a></td><td><a href="http://epi.grants.cancer.gov/staff/reedy.html">reedyj@mail.nih.gov</a></td></tr><tr><td><a href="http://epi.grants.cancer.gov/staff/troiano.html">Richard Troiano</a></td><td><a href="http://epi.grants.cancer.gov/staff/troiano.html">troianor@mail.nih.gov</a></td></tr><tr><td><a href="http://cancercontrol.cancer.gov/od/dep_director_dw.html">Deborah Winn</a></td><td><a href="http://cancercontrol.cancer.gov/od/dep_director_dw.html"> winnde@mail.nih.gov</a></td></tr></tbody></table>'
+	}, {
+		'term': 'healthcare',
+		'title': 'Healthcare Delivery Research',
+		'definition': '',
+		'contacts': '<table summary="Contact information for Research platforms." class="table table-striped table-bordered table-condensed responsive"><thead><tr><th scope="col">Staff Member</th><th scope="col">Email</th></tr></thead><tbody><tr><td><a href="http://healthcaredelivery.cancer.gov/about/staff/bios/doriarose.html">Paul Doria-Rose</a></td><td><a href="http://healthcaredelivery.cancer.gov/about/staff/bios/doriarose.html">doriarop@mail.nih.gov</a></td></tr><tr><td><a href="http://healthcaredelivery.cancer.gov/about/staff/bios/enewold.html">Lindsey Enewold</a></td><td><a href="http://healthcaredelivery.cancer.gov/about/staff/bios/enewold.html">lindsey.enewold@nih.gov</a></td></tr><tr><td><a href="http://healthcaredelivery.cancer.gov/about/staff/bios/geiger.html">Ann Geiger</a></td><td><a href="mailto:geigeram@mail.nih.gov">geigeram@mail.nih.gov</a></td></tr><tr><td><a href="http://healthcaredelivery.cancer.gov/about/staff/bios/kent.html">Erin Kent</a></td><td><a href="http://healthcaredelivery.cancer.gov/about/staff/bios/kent.html">erin.kent@nih.gov</a></td></tr><tr><td><a href="http://healthcaredelivery.cancer.gov/about/staff/bios/rendle.html">Kate Rendel</a></td><td><a href="http://healthcaredelivery.cancer.gov/about/staff/bios/rendle.html">kate.rendle@nih.gov</a></td></tr></tbody></table>'
+	}, {
+		'term': 'implementation-science',
+		'title': 'Implementation Science',
+		'definition': 'Some examples of health behaviors include a healthy diet, regular exercise, not smoking, and following recommended screening guidelines.',
+		'contacts': '<table summary="Contact information for Research platforms." class="table table-striped table-bordered table-condensed responsive"><thead><tr><th scope="col">Staff Member</th><th scope="col">Email</th></tr></thead><tbody><tr><td><a href="http://cancercontrol.cancer.gov/IS/staff/Farrell.html">Margaret Farrell</a></td><td><a href="http://cancercontrol.cancer.gov/IS/staff/Farrell.html">farrellm@mail.nih.gov</a></td></tr><tr><td><a href="https://www.cdc.gov/genomics/about/staff/mkhoury.htm">Muin Khoury</a></td><td><a href="https://www.cdc.gov/genomics/about/staff/mkhoury.htm">muk1@cdc.gov</a></td></tr><tr><td><a href="http://staffprofiles.cancer.gov/brp/prgmStaffProfile.do?contactId=24750065&amp;bioType=flw">Megan Roberts</a></td><td><a href="http://staffprofiles.cancer.gov/brp/prgmStaffProfile.do?contactId=24750065&amp;bioType=flw">megan.roberts@nih.gov</a></td></tr></tbody></table>'
+	}, {
+		'term': 'surveillance-research',
+		'title': 'Surveillance Research',
+		'definition': '',
+		'contacts': '<table summary="Contact information for Research platforms." class="table table-striped table-bordered table-condensed responsive"><thead><tr><th scope="col">Staff Member</th><th scope="col">Email</th></tr></thead><tbody><tr><td><a href="http://epi.grants.cancer.gov/staff/rivera.html">Donna Rivera</a></td><td><a href="http://epi.grants.cancer.gov/staff/rivera.html">donna.rivera@nih.gov</a></td></tr></tbody></table>'
+	}, {
+		'term': 'survivorship-research',
+		'title': 'Survivorship Research',
+		'definition': '',
+		'contacts': '<table class="table table-striped table-bordered table-condensed responsive" summary="Contact information for Research platforms." class="table table-striped table-bordered table-condensed responsive"><thead><tr><th scope="col">Link</th></tr></thead><tbody><tr><td><a href="http://cancercontrol.cancer.gov/ocs/about/staff.html">Expertise in Cancer Survivorship Research</a></td></tr></tbody></table>'
+	}
+];
+
+var windowsize = $(window).width();
+
+$(document).resize(function() {
+  var windowsize = $(window).width();
+});
+
+$(document).ready(function() {
+	var p_div = $('#popup'),
+		a_div = $('#popup-acro'),
+		c_div = $('.contact-info');
+
+	// user interaction (image) - hover
+	$('area').mouseenter(function(e) {
+		var x = $(this).attr('id'),
+			term_content = '',
+			moveLeft = 0,
+			moveDown = 0;
+						
+		if (x === 'psycho-social' || x === 'quality-of-life') {
+			moveLeft = -310,
+			moveDown = 70;
+		} else if (x === 'health-behaviors' || x === 'disease-progression') {
+			moveLeft = -600,
+			moveDown = -225;
+		} else if (x === 'research-platforms') {
+			moveLeft = 50,
+			moveDown = -150;
+		} else if (x === 'special-populations' || x === 'patterns' || x === 'economic-impact') {
+			moveLeft = 20,
+			moveDown = -25;
+		} else {
+			moveLeft = -140,
+			moveDown = 30;
+		}
+		
+		if (windowsize < 768) {
+			if (x === 'health-behaviors' || x === 'disease-progression') {
+			moveLeft = -400,
+			moveDown = -150;
+			} else if (x === 'research-platforms') {
+			moveLeft = 100,
+			moveDown = -250;
+			} else {
+			}
+		}
+
+			if (windowsize > 490) {
+		for(var i = 0; i < terms.length; i++) {
+			if(terms[i].term == x) { term_content += '<h3>' + terms[i].title + '</h3>' + '<p>' + terms[i].definition + '</p>' }
+		}
+		p_div.empty();
+		p_div.append(term_content);
+		p_div.stop(true).fadeTo(0, 1.0);
+
+		$('area').mousemove(function(e) { p_div.css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft) });
+	}});
+	$('area').mouseleave(function(e) { p_div.fadeOut(0) });
+
+	// user interaction - click
+	$('area').click(function() {
+		var y = $(this).attr('id');
+		for(var i = 0; i < terms.length; i++) {
+			if(terms[i].term == y) {
+				contact_content = '<h2>' + terms[i].title + '</h2>';
+				contact_content += terms[i].contacts;
+			}
+		}
+		c_div.empty();
+		c_div.append(contact_content);
+	});
+});
+
+// scroll animation
+$(function() {
+	$('area[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({ scrollTop: target.offset().top }, 1200);
+				return false;
+			}
+		}
+	});
+});
