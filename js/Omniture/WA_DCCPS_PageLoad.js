3b1936c6f5084c651476dbc9f8a376b4c9ffef4c
deloosecr <deloosecr@ncias-d1611-v.nci.nih.gov>
2017-01-07 01:34:05 +0000

    CD: Funding Opportunity updates Former-commit-id: 9fdc5ddf6b11192eb4b00d6b88fd149b10b3d955

--- js/Omniture/WA_DCCPS_PageLoad.js
+++ js/Omniture/WA_DCCPS_PageLoad.js
@@ -1,6 +1,6 @@
-document.write('<script type="text/javascript" src="/JS/Omniture/WA_DCCPS_pre.js" ></script>');
-document.write('<script type="text/javascript" src="/JS/Omniture/s_code.js" ></script>');
-document.write('<script type="text/javascript" src="/JS/Omniture/WA_DCCPS_post.js" ></script>');
+document.write('<script type="text/javascript" src="https://cancercontrol.cancer.gov/JS/Omniture/WA_DCCPS_pre.js" ></script>');
+document.write('<script type="text/javascript" src="https://cancercontrol.cancer.gov/JS/Omniture/s_code.js" ></script>');
+document.write('<script type="text/javascript" src="https://cancercontrol.cancer.gov/JS/Omniture/WA_DCCPS_post.js" ></script>');
 
 
 // Add the line below just before the end body tag to execute Omniture page-load event:
