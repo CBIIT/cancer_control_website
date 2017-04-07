f0a8ff0e2d8de48b34b2440bfd9714076a391ce3
deloosecr <deloosecr@ncias-d1611-v.nci.nih.gov>
2017-04-07 18:55:27 +0000

    Merged revision(s) 906 from branches/ICF
    Former-commit-id: 580163fa78c774436090eb5b6ca7358ed5648bbf

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  "use strict";
+// Smooth scroll back to top
+	$('.scrollToTop').click(function(){
+		$('body,html,document').animate({scrollTop:0}, 'slow');
+		return false;
+	});
+// Hamburger menu to X and Menu text change to Close
+	$('header .navbar-toggle').on('click', function () {
+		$(this).toggleClass('active');
+		if ($('header .nav-menu-title').text() === 'MENU') {
+			$('header .nav-menu-title').text('CLOSE'); 
+		} else { 
+			$('header .nav-menu-title').text('MENU');
+		}
+	});
+// Random homepage hero image	
+	var images = ['heroimagebanner1.jpg', 'heroimagebanner2.jpg', 'heroimagebanner3.jpg', 'heroimagebanner4.jpg'];
+ 	$('#hero-image').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+// Lightbox
+	$('.magnify').click(function() {
+		$(this).next('.modal').css({'display':'block'});
+	});
+	$('.close').click(function() {
+		$(this).parent('.modal').css({'display':'none'});
+	});
+// Accordion +/- toggle	
+	$('.accordion-toggle').click(function() {
+		$(this).find('.glyphicon').toggleClass('glyphicon-minus');
+	});
+});
